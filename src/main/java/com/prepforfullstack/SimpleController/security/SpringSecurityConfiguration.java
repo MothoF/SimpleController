0e41c8ee7421abdc8693cc10bc3437d031322279
Tshepo Majoro <tsmajorjhb024@student.wethinkcode.co.za>
2025-11-24 16:20:51 +0200

    ...

--- src/main/java/com/prepforfullstack/SimpleController/security/SpringSecurityConfiguration.java
+++ src/main/java/com/prepforfullstack/SimpleController/security/SpringSecurityConfiguration.java
@@ -12,17 +12,25 @@ import java.util.function.Function;
 
 @Configuration
 public class SpringSecurityConfiguration {
-    Function<String, String> encodePassword = input ->
-        passwordEncoder().encode(input);
+//    Function<String, String> encodePassword = input ->
+//        passwordEncoder().encode(input);
 
     @Bean
     InMemoryUserDetailsManager inMemoryUserDetailsManager(){
-        UserDetails user = User.builder().passwordEncoder(encodePassword).username("Mothofeela").password("@Mo2f").roles("user").build();
-        return new InMemoryUserDetailsManager(user);
+//        UserDetails user = User.builder().passwordEncoder(encodePassword).username("Mothofeela").password("@Mo2f").roles("user").build();
+        UserDetails user1 = addUser("Mothofeela", "@M02f");
+        UserDetails user2 = addUser("Lakabane","@Lucky");
+        return new InMemoryUserDetailsManager(user1,user2);
     }
 
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    private UserDetails addUser(String username, String password){
+        Function<String, String> encodePassword = input ->
+                passwordEncoder().encode(input);
+        return User.builder().passwordEncoder(encodePassword).username(username).password(password).roles("user").build();
     }
 }
